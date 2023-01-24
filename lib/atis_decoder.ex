b409ed19e7f6bca6a85222c37f7732d24a0c1610
Joe Pettit <jpettit@pettitservers.com>
2023-01-23 21:18:52 -0600

    base

---
+++ lib/atis_decoder.ex
@@ -0,0 +1,18 @@
+defmodule AtisDecoder do
+  @moduledoc """
+  Documentation for `AtisDecoder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AtisDecoder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/atis_decoder_test.exs
@@ -0,0 +1,8 @@
+defmodule AtisDecoderTest do
+  use ExUnit.Case
+  doctest AtisDecoder
+
+  test "greets the world" do
+    assert AtisDecoder.hello() == :world
+  end
+end
